66cc63871887b60b1288c4b3bb07d519ad1a3b50
kostia <viocost@gmail.com>
2018-10-31 20:36:34 +0500

    rc-local  service setup added

--- installer/vmsetup_vmware.sh
+++ installer/vmsetup_vmware.sh
@@ -14,6 +14,44 @@
 #apt install open-vm-tools -y 
 #apt install open-vm-tools-desktop -y
 
+
+# Creating rc.local
+
+echo  "#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+ 
+exit 0
+" >> /etc/rc.local
+
+chmod +x /etc/rc.local
+
+echo "[Unit]
+ Description=/etc/rc.local Compatibility
+ ConditionPathExists=/etc/rc.local
+ 
+[Service]
+ Type=forking
+ ExecStart=/etc/rc.local start
+ TimeoutSec=0
+ StandardOutput=tty
+ RemainAfterExit=yes
+ SysVStartPriority=99
+ 
+[Install]
+ WantedBy=multi-user.target
+" >> /etc/systemd/system/rc-local.service
+
+
 # VMWare automount setup
 echo ".host:/	/mnt/hgfs -o uid=1000 -o gid=1000 -o umask=775	fuse.vmhgfs-fuse	allow_other	0	0" >> /etc/fstab
 echo "mount /mnt/hgfs" >> /etc/rc.local
