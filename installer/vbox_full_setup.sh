458944f9d9a170122d76f252f9e32fc2dad99ca5
Konstantin <viocost@gmail.com>
2019-03-14 13:15:51 +0500

    VBox installer script fix

--- installer/vbox_full_setup.sh
+++ installer/vbox_full_setup.sh
@@ -73,7 +73,7 @@ echo Installing TOR
 echo 'deb https://deb.torproject.org/torproject.org stretch main' | tee -a /etc/apt/sources.list
 echo 'deb-src https://deb.torproject.org/torproject.org stretch  main' | tee -a /etc/apt/sources.list
 
-gpg --no-tty curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
+curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
 gpg --no-tty --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
 
 apt update
