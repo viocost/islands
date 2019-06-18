ed08eece8095ee86bea8e86a2ae4cf8a6e94f6db
kostia <viocost@gmail.com>
2019-06-18 07:16:06 +0500

    debug

--- island-manager/run.sh
+++ island-manager/run.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+# Make sure to run this from Island Manager root directory
+# Assummed that python v3.7 or higher, pip3 and python3-venv are installed
+if [ ! -d "./venv" ]; then
+    echo creating virtual environment
+    mkdir venv
+    python3 -m venv ./venv
+    
+fi
+
+source ./venv/bin/activate 
+pip3 install -r requirements.txt
+
+python3 ./main.py
+
+deactivate
+
