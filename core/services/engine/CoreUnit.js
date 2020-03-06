c01d33f5c3c603aa4ab123adeeecda2dae984977
kostia <viocost@gmail.com>
2020-03-06 15:06:04 +0500

    Restart logic implemented

--- core/services/engine/CoreUnit.js
+++ core/services/engine/CoreUnit.js
@@ -11,20 +11,48 @@ class CoreUnit{
         console.log("Launching core unit: " + cmd)
 
         this.cmd = cmd
-        this.restartTimeout = 0;
-        this.crashes = getLimitedLengthArray(10);
+        this.restartTimeout = 200;
+        this.crashLevel = 0;
+        this.crashes = getLimitedLengthArray(15);
         this.process = null //  process handle
         this.killing = false // kill flag
+    }
+
+    setCrashLevel(level){
+        this.crashLevel = level;
+    }
+
+    calculateRestartTimeout(){
+        let curTime = new Date()
+        let timeoutPerLevel = [200, 2000, 5000]
+        let timeWindow = timeWindow[this.crashLevel] * 15;
+
+        let crashCount = 0;
+
+        for(let ts of this.crashes.reverse()){
+            if (curTime - ts <= timeWindow){
+                crashCount++
+            }
+        }
+
+        if (crashCount >= 10 && this.crashLevel < 2){
+            this.setCrashLevel(this.crashLevel + 1)
+        } else {
+            this.setCrashLevel(0)
+        }
+
+        return timeoutPerLevel[this.crashLevel]
     }
 
     launch(){
         let self = this;
         this.process = exec(this.cmd)
         let handler = ()=>{
+            self.crashes.push(new Date())
                 setTimeout(()=>{
                     if (self.killing) return;
                     self.launch()
-                }, self.restartTimeout)
+                }, self.calculateRestartTimeout())
         }
         this.process.on('close', handler)
         this.process.on('exit', handler)
