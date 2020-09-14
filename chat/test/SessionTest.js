f90556b63b171d755b840c6d615dd4dc289b744f
kostia <viocost@gmail.com>
2020-09-14 13:37:08 +0500

    testing session

--- chat/test/SessionTest.js
+++ chat/test/SessionTest.js
@@ -2,12 +2,14 @@ const { assert, expect } = require("chai")
 const { SessionFactory } = require("../server/lib/Session")
 const { EventEmitter } = require("events")
 
+
+
 class MockConnector extends EventEmitter{
     constructor(){
         super()
     }
-    send(msg){
-        console.log(`Mock connector sends: ${msg}`);
+    send(ev, msg){
+        console.log(`Mock connector sends ev: ${ev}: ${msg}`);
     }
 }
 
@@ -15,19 +17,27 @@ class MockConnector extends EventEmitter{
 class MockCrypto{
     encrypt(msg){
         console.log(`Mock crypto encrypts message ${msg}`);
+        return msg
     }
 
     decrypt(msg){
         console.log(`Mock crypto decrypts message ${msg}`);
+        return msg
     }
 }
 
 
 describe("Session test", ()=>{
+    before(()=>{
+        this.connectorServer = new MockConnector()
+        this.sessionServer = SessionFactory.makeRegularSession(this.connectorServer, new MockCrypto())
+    })
 
     it("should init the session with mock objects", ()=>{
-        let session = SessionFactory.makeRegularSession(new MockConnector(), new MockCrypto())
-        session.acceptMessage("booo")
+        this.sessionServer.acceptMessage("booo")
+        this.sessionServer.acceptMessage({a: 1, b: "asdf"})
+        this.sessionServer.acceptMessage("goo")
+        this.sessionServer.acceptMessage([1, 2, 3, 4, 5])
 
     })
 })
